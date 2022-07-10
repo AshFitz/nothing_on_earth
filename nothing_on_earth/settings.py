d3bda94a83e7140a10808cbd25b8df3812d87acb
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-10 19:44:20 +0000

    Update static file dirs

--- nothing_on_earth/settings.py
+++ nothing_on_earth/settings.py
@@ -149,10 +149,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # It will be deactivated when running collectstatic. Active on localhost
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/') # It will be activated when running collectstatic. Active on server
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+
+#Media Folder settings
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
