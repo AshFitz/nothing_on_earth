4d4b1a3b6c98ec26cda450bc71a6881bbb1d924a
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-17 14:17:47 +0000

    Update context functionality to accept sizes

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,15 +12,17 @@ def bag_contents(request):
     grand_total = delivery + total
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
+    for item_id, item_data in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+        for size, item_data in item_data['items_by_size'].items():
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'item_data': item_data,
+                'product': product,  
+                'size': size,
+            })
 
     context = {
         'bag_items': bag_items,
