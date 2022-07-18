860a385eda94e627d46aa6e1d66c1b4003334494
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-18 22:43:52 +0000

    Remove for loop to resolve error object not subscriptable

--- bag/contexts.py
+++ bag/contexts.py
@@ -14,14 +14,14 @@ def bag_contents(request):
 
     for item_id, item_data in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        for size, item_data in item_data['items_by_size'].items():
-            total += item_data * product.price
-            product_count += item_data
-            bag_items.append({
-                'item_id': item_id,
-                'item_data': item_data,
-                'product': product,  
-                'size': size,
+
+        total += item_data * product.price
+        product_count += item_data
+        bag_items.append({
+            'item_id': item_id,
+            'item_data': item_data,
+            'product': product,  
+            
             })
 
     context = {
