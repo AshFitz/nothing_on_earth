d7d3367acbac2eae3672041b3936879f7145eec3
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-23 15:16:27 +0000

    Add public key and test secret key

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L2hGJKW8hjxRzk2U8TUMAQ47r3qkGjCGie8h50YzmmKQfIIZhcOZJWzpo1BT7FpsN7oGeGHz2ALfjIq07WG9Sz9009T9Im3gI',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
