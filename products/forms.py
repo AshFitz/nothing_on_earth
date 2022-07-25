c5c288a9d399731704dfb5e67b6389f656c6b524
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-25 22:34:46 +0000

    Updated categories to collections

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, Category
+from .models import Product, Collection
 
 
 class ProductForm(forms.ModelForm):
@@ -10,9 +10,9 @@ class ProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        collection = Collection.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in collection]
 
-        self.fields['category'].choices = friendly_names
+        self.fields['collection'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-dark rounded-0'
