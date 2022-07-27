5dd3acdb65dbf84ed663e558965c63e15c8b4d21
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-27 17:47:09 +0000

    Remove secret key and set debug

--- nothing_on_earth/settings.py
+++ nothing_on_earth/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k2k-y=a%6mc=k*ctuhtu7_7(&ze%&2pkr+$tw5)7!13n289n)h'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 # Update allowed hosts
 ALLOWED_HOSTS = ['nothing-on-earth.herokuapp.com', 'localhost', '8000-ashfitz-nothingonearth-vqmm1spyesz.ws-eu54.gitpod.io/']
