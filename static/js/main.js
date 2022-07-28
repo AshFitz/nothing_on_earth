9d9cef51d37c7f416d9901a7ef88eab0f00b90b0
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-28 20:02:04 +0000

    Add main js file & on click func to show & hide nav

---
+++ static/js/main.js
@@ -0,0 +1,16 @@
+const mobileBurger = $('#burger-menu-icon'); 
+const mobileNav = $('.mobile-nav-side');
+const navBackground = $('.nav-background');
+
+mobileBurger.click(function(){
+    navBackground.show();
+    navBackground.addClass('overlay-nav');
+    mobileNav.addClass('transition-right');
+    return false;
+});
+
+
+navBackground.click(function(){
+    navBackground.removeClass('overlay-nav');
+    mobileNav.removeClass('transition-right');
+})
