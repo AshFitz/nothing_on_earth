ef85e60426030af14bd0cb5d3836b7055a380901
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-29 21:18:10 +0000

    Add empty field as choice in Collection

--- products/forms.py
+++ products/forms.py
@@ -17,6 +17,6 @@ class ProductForm(forms.ModelForm):
         collection = Collection.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in collection]
 
-        self.fields['collection'].choices = friendly_names
+        self.fields['collection'].choices = [('', '')] + friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-dark rounded-0'
