79b008485b15bac4f5baef7c6d3a1abaa473e9a6
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-30 16:07:37 +0000

    Create new model to handle a review from the user

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Collection(models.Model):
     name = models.CharField(max_length=254,null=True, blank=True)
@@ -28,3 +28,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,
+                                null=True, blank=True,related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True,
+                            related_name='user_review')
+    comment = models.TextField(max_length=1000, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
