e323376c7fef4e122ee1f39426c45f7162ecd59e
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-30 16:08:43 +0000

    Create a form for the review , store the form to db.

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .widgets import CustomClearableFileInput
-from .models import Product, Collection
+from .models import Product, Collection, Review
 
 
 class ProductForm(forms.ModelForm):
@@ -20,3 +20,22 @@ class ProductForm(forms.ModelForm):
         self.fields['collection'].choices = [('', '')] + friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-dark rounded-0'
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['comment']
+        widgets = {
+            'comment': forms.Textarea(
+                attrs={
+                    "rows": 5,
+                    "class": 'form-control stripe-style-input',
+                    "placeholder": "Minimum 20 Characters",
+                }
+            )
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].label = ""
