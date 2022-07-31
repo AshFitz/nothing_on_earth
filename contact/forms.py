f58655653b4c7efadfc57f0ae3a05aa712dce9d5
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-31 22:55:07 +0000

    Adjust spacing

--- contact/forms.py
+++ contact/forms.py
@@ -6,22 +6,22 @@ class Contact(forms.ModelForm):
         model = ContactForm
         fields = ('first_name', 'last_name', 'email', 'phone', 'message')
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            placeholders = {
-                'first_name': 'First Name',
-                'last_name': 'Last Name',
-                'email': 'Email',
-                'phone': 'Contact Number',
-                'message': 'Your Message',
-            }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email',
+            'phone': 'Contact Number',
+            'message': 'Your Message',
+        }
 
-            self.fields['first_name'].widget.attrs['autofocus'] = True
-            for field in self.fields:
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-                self.fields[field].label = False
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
