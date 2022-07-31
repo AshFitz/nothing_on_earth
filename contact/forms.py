b57de5016832eaf61945761e0e532186fbe1fe9c
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-31 00:52:57 +0000

    Add contact form and placeholder fields

---
+++ contact/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import ContactForm
+
+class Contact(forms.ModelForm):
+    class Meta:
+        model = ContactForm
+        fields = ('first_name', 'last_name', 'email', 'phone', 'message')
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            placeholders = {
+                'first_name': 'First Name',
+                'last_name': 'Last Name',
+                'email': 'Email',
+                'phone': 'Contact Number',
+                'message': 'Your Message',
+            }
+
+            self.fields['first_name'].widget.attrs['autofocus'] = True
+            for field in self.fields:
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+                self.fields[field].label = False
