ec8c7e6b02735142194a539fabacd7df104625bd
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-31 00:52:11 +0000

    Add contact models

---
+++ contact/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class ContactForm(models.Model):
+    first_name = models.CharField(max_length=80, blank=False, null=False)
+    last_name = models.CharField(max_length=80, blank=False, null=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone = models.CharField(max_length=20, null=False, blank=False, default='')
+    message = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.message}'
