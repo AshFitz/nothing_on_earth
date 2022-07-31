be08f9d226270610e914ef651a23c03d1717d429
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-07-31 22:57:51 +0000

    Add timestamp to model, to show when user reviewed a product

--- products/models.py
+++ products/models.py
@@ -35,6 +35,7 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True,
                             related_name='user_review')
     comment = models.TextField(max_length=1000, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.username
