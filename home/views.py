ee856968924942b3f4cd4cd96183896b390775f1
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-08-02 08:22:05 +0000

    Fix formatting in home app

--- home/views.py
+++ home/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from products.models import Product, Collection
+
 # Create your views here.
 
+
 def index(request):
-    """ A view to return the index page """
+    """A view to return the index page"""
     products = Product.objects.all()
 
-    context = {
-        "products": products
-    }
-    return render(request, 'home/index.html', context)
+    context = {"products": products}
+    return render(request, "home/index.html", context)
