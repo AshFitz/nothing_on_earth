d7a227896888682af49d3142f9ccf4dfb09456e1
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-08-02 08:23:07 +0000

    Fix formatting checkout app

--- checkout/apps.py
+++ checkout/apps.py
@@ -5,8 +5,9 @@ class CheckoutConfig(AppConfig):
     """
     Checkout name and handle the ready function
     """
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'checkout'
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "checkout"
 
     def ready(self):
         import checkout.signals
